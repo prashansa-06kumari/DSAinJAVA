df65cfecf54011ae04793467cc506d8b8c41cd21
prashansa-06kumari <158124679+prashansa-06kumari@users.noreply.github.com>
2025-02-21 22:23:24 +0530

    Add files via upload

---
+++ Celing.java
@@ -0,0 +1,35 @@
+public class Celing {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18};
+        int target = 15;
+        int ans = celing(arr, target);
+        System.out.println(ans);
+
+    }
+
+    static int celing(int[] arr,int target){
+
+        //but what if the target is greater than the greater number in the array
+
+        if(target > arr[arr.length - 1]){
+            return -1;
+        }
+
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target<arr[mid]){
+                end = mid - 1;
+            }else if(target > arr[mid]){
+                start = mid+1;
+
+            }else{
+                return mid;
+            }
+        }
+        return start;
+    }
+}
