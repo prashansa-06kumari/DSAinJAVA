2d947ac05a1680eaf1b0ed716350b88886d7b495
Prashansa Kumari <158124679+prashansa-06kumari@users.noreply.github.com>
2025-03-21 23:46:15 +0530

    Add files via upload

---
+++ CLL.java
@@ -0,0 +1,86 @@
+package LinkedListt;
+
+public class CLL {
+
+    private Node head;
+    private Node tail;
+
+    public CLL(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insert(int val){
+        Node node = new Node(val);
+        if(head == null){
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
+
+    public void display(){
+        Node node = head;
+        if(head != null){
+            do{
+                System.out.print(node.val + " -> ");
+                node = node.next;
+            }while(node != head);
+        }
+        System.out.println("HEAD");
+    }
+
+    //to delete
+    public void delete(int val){
+        Node node = head;
+        if(node == null){
+            return;
+        }
+
+        if(node.val == val){
+            head = head.next;
+            tail.next = head;
+            return;
+        }
+
+        do{
+            Node n = node.next;
+            if(n.val == val){
+                node.next = n.next;
+                break;
+            }
+            node = node.next;
+        }while(node != head);
+    }
+
+    private class Node{
+        int val;
+        Node next;
+
+        public Node(int val){
+            this.val = val;
+        }
+    }
+}
+
+
+
+class Main8{
+    public static void main(String[] args) {
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+
+        list.display();
+
+        list.delete(19);
+
+        list.display();
+    }
+}
+
