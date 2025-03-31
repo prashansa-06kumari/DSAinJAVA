fdd9ce0eac111b210a8901c4d5c19227f1b9068e
Prashansa Kumari <158124679+prashansa-06kumari@users.noreply.github.com>
2025-03-31 19:20:19 +0530

    Add files via upload

--- displayStack.java
+++ displayStack.java
@@ -3,24 +3,23 @@ package Stack;
 import java.util.Stack;
 
 public class displayStack {
+
     public static void displayReverseRec(Stack<Integer> s){
         if(s.size() == 0) return;
         int top = s.pop();
-        System.out.print(top+" ");
+        System.out.println(top+" ");
         displayReverseRec(s);
         s.push(top);
-
+        System.out.println();
     }
+
     public static void displayRec(Stack<Integer> s){
         if(s.size() == 0) return;
         int top = s.pop();
-        displayReverseRec(s);
-        System.out.println(top+" ");
-
+        displayRec(s);
+        System.out.println(top);
         s.push(top);
-
     }
-
 
 
     public static void main(String[] args) {
@@ -30,35 +29,9 @@ public class displayStack {
         st.push(3);
         st.push(4);
         st.push(5);
-//        System.out.println(st);
+
         displayReverseRec(st);
         displayRec(st);
-//        System.out.println(st);
-
-        //through stack
-//        Stack<Integer> rt = new Stack<>();
-//        while(st.size() > 0){
-//            rt.push(st.pop());
-//
-//        }
-//        while(rt.size() > 0){
-//            int x = rt.pop();
-//            System.out.println(x + " ");
-//            st.push(x);
-//        }
-
-        //through array
-//        int n = st.size();
-//        int[] arr = new int[n];
-//        for(int i = n - 1; i >= 0; i--){
-//            arr[i] = st.pop();
-//
-//        }
-//        for(int i = 0; i<n; i++){
-//            System.out.print(arr[i]+" ");
-//            st.push(arr[i]);
-//        }
-
     }
 
 }
